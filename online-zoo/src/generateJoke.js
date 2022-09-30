18af9d9675b51585063f81ee83aa0d30bfe71c43
Tati-Zhurr <zhtv88@gmail.com>
2022-09-30 16:19:13 +0200

    feat: page About and page Donate

--- online-zoo/src/generateJoke.js
+++ online-zoo/src/generateJoke.js
@@ -1,3 +1,6 @@
+
+let buttonFooter= document.querySelector('.button__footer');
+
 function generateJoke(){
     console.log('Can you check my work later on Thurthday please? I will do a little bit more. Thanks in advance!')
 }
