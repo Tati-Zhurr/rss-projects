3dbccc2c7e67bfb4a9502826c5a6ba60106edad2
Tati-Zhurr <zhtv88@gmail.com>
2022-10-03 23:42:16 +0200

    feat: 1000px width and adoptive

--- online-zoo/src/generateJoke.js
+++ online-zoo/src/generateJoke.js
@@ -2,7 +2,7 @@
 let buttonFooter= document.querySelector('.button__footer');
 
 function generateJoke(){
-    console.log('Can you check my work later on Thurthday please? I will do a little bit more. Thanks in advance!')
+    console.log('Can you check my work on the last day of cross-check please? I will do a little bit more. Thanks in advance!')
 }
 
 export default generateJoke;
