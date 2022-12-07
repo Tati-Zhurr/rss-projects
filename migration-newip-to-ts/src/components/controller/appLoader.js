0a1939c9c13bb0d974173600aa327bc3df8daf80
Tati-Zhurr <zhtv88@gmail.com>
2022-12-07 15:39:17 +0100

    init: start task migration-newip-to-ts

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '1274e0bdac75415580bc714152baaefb', // получите свой ключ https://newsapi.org/
         });
     }
 }
--- migration-newip-to-ts/webpack.config.js
+++ migration-newip-to-ts/webpack.config.js
@@ -3,24 +3,81 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+/*const jsLoaders =() =>{
+    const loaders = [{
+        loader: 'babel-loader',
+        options: {
+            presets: ['@babel/preset-env']
+                  }
+    }]
+
+    if (isDev) {
+        loaders.push('eslint-loader')
+    }
+
+    return loaders;
+}*/
+
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
     mode: 'development',
+    entry: './src/index.ts',
+    module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: ['@babel/polyfill', 'bundle.js'],
+    path: path.resolve(__dirname, 'dist'),
+  },
+   entry: {
+        bundle: ['@babel/polyfill', path.resolve(__dirname,'src/index.js')]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name][contenthash].js',
+        clean: true,
+        assetModuleFilename: '[name][ext]',
+    },
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname,'dist')
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+       
+    },
     module: {
         rules: [
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+              }
         ],
     },
-    resolve: {
+   /* resolve: {
         extensions: ['.js'],
-    },
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, '../dist'),
-    },
+    },*/
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, './src/index.html'),
