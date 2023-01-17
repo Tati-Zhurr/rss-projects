4d36bfd00305bfd529c33927c144d690676a1c83
Tati-Zhurr <zhtv88@gmail.com>
2023-01-17 22:27:23 +0100

    feat: add header for garage page from ts

--- async-race/src/components/app/resetFilters.ts
+++
@@ -1,7 +0,0 @@
-function resetFilters() {
-    window.location.href = `${window.location.pathname}`;
-    localStorage.clear();
-}
-export default resetFilters;
-
-//window.location.href = `${location.origin}/dist/card.html`;
