43ccedeb17b05868a7b713d60cca277d3091353a
Tati-Zhurr <106331574+Tati-Zhurr@users.noreply.github.com>
2023-05-04 13:22:49 +0200

    Delete generateJoke.js

--- online-zoo/src/generateJoke.js
+++
@@ -1,8 +0,0 @@
-
-let buttonFooter= document.querySelector('.button__footer');
-
-function generateJoke(){
-    console.log('Can you check my work a little bit later please? I will correct some bits. Thanks in advance!')
-}
-
-export default generateJoke;
